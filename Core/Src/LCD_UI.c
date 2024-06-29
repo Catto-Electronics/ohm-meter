25bf1cd40b7e6e5d7b74b41f8dacc5dd6edac1cb
rafa-2112 <rafael.reyes.0018@gmail.com>
2024-06-29 18:09:14 -0500

    Char array size

--- Core/Src/LCD_UI.c
+++ Core/Src/LCD_UI.c
@@ -8,7 +8,7 @@ extern R_paramTypeDef R_config;
 void truncate(double resistor_value, double standard_value);
 
 
-char writeString[16];
+char writeString[20];
 char R_Val[7];
 char E_Val[5];
 
@@ -36,10 +36,10 @@ void screenMeasurements(void)
 {
 	truncate(R_config.r_measured, R_config.r_standard);
 	lcd_put_cur(0, 0);
-	sprintf(writeString, "Value:%6s          ", R_Val);
+	sprintf(writeString, "Value:%6s       ", R_Val);
 	lcd_send_string(writeString);
 	lcd_put_cur(1, 0);
-	sprintf(writeString, "EVal:%4s Er:%.1f%%    ", E_Val, R_config.r_percentage);
+	sprintf(writeString, "EVal:%4s   Er:%.1f%%", E_Val, R_config.r_percentage);
 	lcd_send_string(writeString);
 	HAL_Delay(500);
 }
@@ -49,23 +49,25 @@ void truncate(double resistor_value, double standard_value)
 {
 
 	if(resistor_value >= 1000)
-	{
 		sprintf(R_Val, "%.2fk", resistor_value / 1000);
-	}
 
 	if(resistor_value >= 1000000)
-	{
 		sprintf(R_Val, "%.2fM", resistor_value / 1000000);
-	}
 
 	if(resistor_value >= 1000)
 	{
 		sprintf(E_Val, "%.1fk", standard_value/1000);
+
+		if(resistor_value >= 10000)
+			sprintf(E_Val, "%.0fk", standard_value/1000);
 	}
 
 	if(resistor_value >= 1000000)
 	{
 		sprintf(E_Val, "%.1fM", standard_value/1000000);
+
+		if(resistor_value >= 10000000)
+			sprintf(E_Val, "%.0fk", standard_value/1000000);
 	}
 
 }
